524fbe47fb76970f6430cc41250a4e9409f165c0
TimSch1 <timothy.schmeier@gmail.com>
2015-03-21 19:55:36 -0400

    heatmap added

--- Otto.R
+++ Otto.R
@@ -19,11 +19,14 @@ qplot(target, n, data=classFreq, stat='identity', geom='bar')
 library(reshape2)
 library(ggplot2)
 train_m = melt(train, id=c('target','id'), class = variable)
+
+#plot range/max values
 feat.ranges = ddply(train_m, .(target, variable), function(x) range(x$value))
 ggplot(feat.ranges, aes(x=variable, y=V2, group=target, fill=target, label=variable))+xlab('')+
   geom_bar(stat='identity')+facet_wrap(~target)+ylab('Feature Range')+theme_bw()+geom_text()+
   theme(axis.text.x=element_blank())
 
+#plot typical values (mean & median)
 feat.centers = ddply(train_m, .(target, variable), summarize, Mean = mean(value), Median = median(value))
 ggplot(feat.centers, aes(x=variable, y=Mean, group=target, fill=target, label=variable))+xlab('')+
   geom_bar(stat='identity')+facet_wrap(~target)+ylab('Feature Mean')+theme_bw()+geom_text()+
@@ -33,8 +36,20 @@ ggplot(feat.centers, aes(x=variable, y=Median, group=target, fill=target, label=
   geom_bar(stat='identity')+facet_wrap(~target)+ylab('Feature Median')+theme_bw()+geom_text()+
   theme(axis.text.x=element_blank())
 
-library(gplots)
-train2 = scale(as.matrix(train[,-c(94,95)]), center=T, scale=T)
-rownames(train2) = seq(1,nrow(train2),1)
-palette = colorRampPalette(c('lightyellow','darkred'), space='rgb')(100)
-heatmap.2(train2, Rowv=NA, Colv=NA, col=palette)
+#heat map
+Col.Scale = colorRampPalette(colors=c('blue','white','red'))(5)
+heat.Med = spread(feat.centers[,-3],variable, Median)[,-1]
+
+#with all features
+heatmap.2(data.matrix(heat.Med), Rowv=TRUE, Colv=FALSE, scale='column', trace='none', 
+          col=Col.Scale, margins = c(3,5), ylab='Class', xlab='Feature Median')
+
+#get only relevant features w/ median > 0
+idx = which(colSums(heat.Med)!=0)
+heat.Med.small = select(heat.Med, idx)
+
+#condensed heatmap
+dev.off()
+heatmap.2(data.matrix(heat.Med.small), Rowv=TRUE, Colv=FALSE, scale='column', trace='none', 
+          col=Col.Scale, margins = c(3,5), ylab='Class', xlab='Feature Median')
+
